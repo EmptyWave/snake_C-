34633d061192eba41cb0e28ea17c293d6e4da4c6
EmptyWave <dimonyud@gmail.com>
2018-08-25 13:52:41 +0300

    add parent class figure

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,20 +10,30 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1,3,'*');
-            p1.Draw();
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4,5,'#');
-            p2.Draw();
+            //Отрисовка рамочки
+            FrameDraw();
 
-            HorizontalLine hline = new HorizontalLine(5,10,8,'+');
-            hline.Draw();
-
-            VerticalLine vline = new VerticalLine(5, 2, 8, '+');
-            vline.Draw();
+            //drowing points
+            Point p = new Point(4, 5, '*');
+            p.Draw();
 
             Console.ReadLine();
 
+        }
+        
+        public static void FrameDraw()
+        {
+            HorizontalLine HLeftLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine HRightLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine VUpLine = new VerticalLine(0, 0, 24, '+');
+            VerticalLine VDownLine = new VerticalLine(78, 0, 24, '+');
+
+            HLeftLine.Draw();
+            HRightLine.Draw();
+            VUpLine.Draw();
+            VDownLine.Draw();
         }
     }
 }
